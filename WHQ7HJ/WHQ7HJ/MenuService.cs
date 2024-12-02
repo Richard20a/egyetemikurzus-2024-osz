155553c828eda3ef8a938dbb113edae8f72d1839
Richard20a <mrichard932@gmail.com>
2024-12-02 13:45:21 +0100

    Adatkezelő osztályok létrehozása

---
+++ WHQ7HJ/WHQ7HJ/MenuService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WHQ7HJ
+{
+    internal class MenuService
+    {
+        private readonly string _filePath = "menu.json";
+
+
+        public List<MenuItem> LoadMenu()
+        {
+            try
+            {
+                string json = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<MenuItem>>(json) ?? new List<MenuItem>();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<MenuItem>();
+            }
+
+            
+        }
+
+    }
+}
---
+++ WHQ7HJ/WHQ7HJ/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WHQ7HJ
+{
+    internal class OrderService
+    {
+
+        private readonly string _filePath = "orders.json";
+
+
+        public List<MenuItem> LoadOrders()
+        {
+            try
+            {
+                string json = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<MenuItem>>(json) ?? new List<MenuItem>();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<MenuItem>();
+            }
+
+
+        }
+
+    }
+}
